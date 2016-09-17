095a2e4e09640f0550a42654dd9da08c49b03b14
Wingez <gustav.ledous@gmail.com>
2016-09-17 20:21:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+
+print("Number of values: ")
+numberOfValues = int(input())
+
+xValues = []
+yValues = []
+
+for i in range(numberOfValues):
+    print("X"+str(i)+":")
+    xValues.append(float(input()))
+
+for i in range(numberOfValues):
+    print("Y"+str(i)+":")
+    yValues.append(float(input()))
+
+print("Enter minimum value for coefficient")
+minVal = float(input())
+
+print("Enter maximum value for coefficient")
+maxVal = float(input())
+
+print("Enter step")
+step = float(input())
+
+
+leastSquare = None
+bestValue = 0
+
+c = minVal
+
+while c < maxVal:
+    sumOfSquares = 0.0
+    for i in range(numberOfValues):
+        sumOfSquares += (c*xValues[i]**2 - yValues[i])**2
+
+    if leastSquare is None or sumOfSquares < leastSquare:
+        leastSquare = sumOfSquares
+        bestValue = c
+        
+    c+=step
+
+print("y = "+str(bestValue)+"x^2 fits the data best")
